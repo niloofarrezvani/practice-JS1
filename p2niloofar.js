7295336ae533d641aa6be55eb11c68a5f744256a
niloofarrezvani <134488766+niloofarrezvani@users.noreply.github.com>
2023-08-18 23:46:52 +0330

    Add files via upload

---
+++ p2niloofar.js
@@ -0,0 +1,4 @@
+const firstName=prompt("write your first name: ");
+const lastName=prompt("write your last name: ");
+const fullNameLength = firstName.length + lastName.length;
+console.log(fullNameLength);
---
+++ practice3.js
@@ -0,0 +1,5 @@
+const number=prompt("type a number please: ");
+for( let i =0; i<=number;i++){
+    if(i % 2===0)
+    console.log(i)
+}
